e445749a364fde209d80c87aa389af00bc7fe7e3
Mihai Bundea <mihaibundea1@gmail.com>
2024-05-07 23:39:26 +0300

    Introduced graph, PDF saving and details

--- GraphicsView.py
+++ GraphicsView.py
@@ -1,20 +1,10 @@
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QGraphicsView, QApplication
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtGui import QPainter
 
-class GraphicsView(QGraphicsView):
+class GraphicsView(QtWidgets.QGraphicsView):
     photoClicked = QtCore.pyqtSignal(QtCore.QPoint)
 
     def __init__(self, parent):
         super(GraphicsView, self).__init__(parent)
-        # # self.setRenderHint(QPainter.Antialiasing)
-        # # self.setRenderHint(QPainter.SmoothPixmapTransform)
-        # # self.setDragMode(QGraphicsView.ScrollHandDrag) # zooms out incontrollably even if i cant drag and drop
-        # self.setOptimizationFlags(QGraphicsView.DontSavePainterState)
-        # self.setViewportUpdateMode(QGraphicsView.SmartViewportUpdate)
-        # self.setTransformationAnchor(QGraphicsView.AnchorUnderMouse)
-
         self._zoom = 0
         self._empty = True
         self._scene = QtWidgets.QGraphicsScene(self)
@@ -25,23 +15,18 @@ class GraphicsView(QGraphicsView):
         self.setResizeAnchor(QtWidgets.QGraphicsView.AnchorUnderMouse)
         self.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
         self.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+        self.setBackgroundBrush(QtGui.QBrush(QtGui.QColor(30, 30, 30)))
         self.setFrameShape(QtWidgets.QFrame.NoFrame)
-        # to make the background black
-        # self.setBackgroundBrush(QtGui.QBrush(QtGui.QColor(30, 30, 30)))
 
     def hasPhoto(self):
         return not self._empty
 
-
-    # trebuie gasita  o alta metoda pentru ca functia asta nu e nicioadata apelata pentru a face 'resize'
     def fitInView(self, scale=True):
         rect = QtCore.QRectF(self._photo.pixmap().rect())
         if not rect.isNull():
             self.setSceneRect(rect)
             if self.hasPhoto():
                 unity = self.transform().mapRect(QtCore.QRectF(0, 0, 1, 1))
-                print(unity.width())
-                print(unity.height())
                 self.scale(1 / unity.width(), 1 / unity.height())
                 viewrect = self.viewport().rect()
                 scenerect = self.transform().mapRect(rect)
@@ -50,9 +35,17 @@ class GraphicsView(QGraphicsView):
                 self.scale(factor, factor)
             self._zoom = 0
 
-    def resizeEvent(self, event):
+    def setPhoto(self, pixmap=None):
+        self._zoom = 0
+        if pixmap and not pixmap.isNull():
+            self._empty = False
+            self.setDragMode(QtWidgets.QGraphicsView.ScrollHandDrag)
+            self._photo.setPixmap(pixmap)
+        else:
+            self._empty = True
+            self.setDragMode(QtWidgets.QGraphicsView.NoDrag)
+            self._photo.setPixmap(QtGui.QPixmap())
         self.fitInView()
-        super(GraphicsView, self).resizeEvent(event)
 
     def wheelEvent(self, event):
         if self.hasPhoto():
--- widget.py
+++ widget.py
@@ -1,17 +1,33 @@
 import os
-import cv2
+import sys
+
+import resource_rc
 import callable
-from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QListWidget, QLabel, QVBoxLayout, QHBoxLayout, QPushButton, QWidget, QGraphicsView, QGraphicsScene, QMenuBar, QMenu, QAction
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QListWidget, QLabel, QVBoxLayout, QHBoxLayout, QPushButton, QListWidgetItem, QWidget, QGraphicsView, QGraphicsScene, QMenuBar, QMenu, QAction
 from PyQt5.QtGui import QImage, QPixmap
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 import matplotlib.pyplot as plt
-import GraphicsView
-
 from PyQt5.QtWidgets import QMainWindow, QHBoxLayout, QVBoxLayout, QLabel, QListWidget, QGraphicsScene, QWidget
 from GraphicsView import GraphicsView
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import QSize, Qt
 
 from matplotlib.figure import Figure
+from matplotlib.animation import FuncAnimation
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
+from reportlab.lib import utils
+
+class Property:
+    def __init__(self, name):
+        self.name = name
+
+    def change_name(self, new_name):
+        self.name = new_name
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -19,20 +35,23 @@ class MainWindow(QMainWindow):
 
         self.setWindowTitle("Image Viewer and Graphic Generator")
         self.setGeometry(0, 0, 1920, 1080)
-
+        self.showMaximized()
         self.create_menu()
 
         self.folder_path = None
 
         self.main_layout = QHBoxLayout()
         self.layout = QVBoxLayout()
-        self.leftVerticalLayout = QVBoxLayout()
+        self.centerVerticalLayout = QVBoxLayout()
+        self.rightVerticalLayout = QVBoxLayout()
 
         self.path_label = QLabel()
+        self.path_label.setStyleSheet("border: 1px solid rgb(60, 60, 60);")
         self.layout.addWidget(self.path_label)
 
         self.file_listbox = QListWidget()
         self.file_listbox.itemClicked.connect(self.load_selected_image)
+        self.file_listbox.setStyleSheet("border: 1px solid rgb(60, 60, 60);")
         self.file_listbox.setMaximumWidth(250)
         self.layout.addWidget(self.file_listbox)
 
@@ -40,28 +59,119 @@ class MainWindow(QMainWindow):
 
         self.image_view = GraphicsView(self)
         self.image_view.setMinimumWidth(1000)
-        self.main_layout.addWidget(self.image_view)
+        self.image_view.photoClicked.connect(self.photoClicked)
+        self.image_view.setStyleSheet("border: 1px solid rgb(60, 60, 60);")
+
+        self.buttonMove = QPushButton()
+        self.buttonMove.setIcon(QIcon(":/moveIcon.png"))  # replace with your image path
+        self.buttonMove.setIconSize(QSize(32, 32))  # set the icon size to 32x32 pixels
+
+        # Set the QPushButton size to 32x32 pixels
+        self.buttonMove.setFixedSize(QSize(32, 32))
+        self.buttonMove.setStyleSheet("border: 1px solid rgb(60, 60, 60);")
+
+
+        # Make the QPushButton toggleable
+        self.buttonMove.setCheckable(True)
+        self.buttonMove.clicked.connect(self.pixInfo)
+        self.centerVerticalLayout.addWidget(self.buttonMove)
+
+        self.centerVerticalLayout.addWidget(self.image_view)
+        self.main_layout.addLayout(self.centerVerticalLayout)
 
         self.fig = Figure(figsize=(6, 3), facecolor="none")
         self.canvas = FigureCanvas(self.fig)
         self.canvas.setMaximumSize(400,400)
+        self.canvas.setStyleSheet("border: 1px solid rgb(60, 60, 60);")
+
         # Create an axes instance in your figure
 
-        self.leftVerticalLayout.addWidget(self.canvas)
+        self.rightVerticalLayout.addWidget(self.canvas)
+
+        self.saveButton = QPushButton("Save Investigation Details", self)
+        self.saveButton.setStyleSheet("border: 1px solid rgb(60, 60, 60);font-size: 20px;")
+        self.saveButton.setMinimumHeight(100)
+        self.saveButton.clicked.connect(self.save_to_pdf)
+
+        self.rightVerticalLayout.addWidget(self.saveButton)
 
         # Create a QListWidget for the photo details
         self.photo_details_listbox = QListWidget()
         self.photo_details_listbox.setMaximumWidth(400)
+        self.photo_details_listbox.setStyleSheet("border: 1px solid rgb(60, 60, 60); font-size: 20px;")
 
-        self.leftVerticalLayout.addWidget(self.photo_details_listbox)
+        properties = [Property("Result: "), Property("Image size: "), Property("Position of cursor: ")]
 
-        self.main_layout.addLayout(self.leftVerticalLayout)
+        # Add items to the QListWidget
+        for prop in properties:
+            item = QListWidgetItem(prop.name)
+            item.setData(Qt.UserRole, prop)
+            self.photo_details_listbox.addItem(item)
+
+        self.rightVerticalLayout.addWidget(self.photo_details_listbox)
+
+        self.main_layout.addLayout(self.rightVerticalLayout)
 
         # final doings
         self.central_widget = QWidget()
         self.central_widget.setLayout(self.main_layout)
         self.setCentralWidget(self.central_widget)
-        # self.setStyleSheet("background-color: rgb(30, 30, 30); color: white;")
+        self.setStyleSheet("background-color: rgb(30, 30, 30); color: white;")
+
+    def save_to_pdf(self):
+        # Prompt user to select file location
+        file_path, _ = QFileDialog.getSaveFileName(self, "Save PDF", "", "PDF Files (*.pdf)")
+        if not file_path:  # User cancelled
+            return
+
+        # Create a PDF document
+        c = canvas.Canvas(file_path, pagesize=letter)
+        c.setFillColor("black")  # Set text color to black
+
+        # Draw "Investigation Details" centered horizontally
+        c.drawString((letter[0] - c.stringWidth("Investigation Details", "Helvetica", 12)) / 2, 750, "Investigation Details")
+
+        # Draw text from self.photo_details_listbox centered horizontally
+        text = "\n".join(self.photo_details_listbox.item(i).text() for i in range(self.photo_details_listbox.count()))
+        lines = text.split("\n")
+        line_height = 12  # Assuming font size is 12
+        y_position = 600
+        for line in lines:
+            c.drawString(100, y_position, line)
+            y_position -= line_height
+
+        # Add graph from self.canvas on the first page
+        graph_path = "temp_graph.png"
+        ax = self.fig.add_subplot(111, facecolor='none')
+        ax.tick_params(colors='black')
+        ax.xaxis.label.set_color('black')
+        ax.yaxis.label.set_color('black')
+        self.fig.savefig(graph_path)
+        c.drawImage(graph_path, (letter[0] - 400) / 2, 100, width=400, height=300)  # Center the graph
+
+        c.showPage()  # Start a new page
+
+        # Add image from self.image_view on the second page
+        pixmap = self.image_view.grab()
+        image_path = "temp_image.png"
+        pixmap.save(image_path)
+        c.drawImage(image_path, (letter[0] - pixmap.width() / 3) / 2, 200, width=pixmap.width() / 3, height=pixmap.height() / 3)  # Center the image
+
+        c.save()
+
+
+
+    def pixInfo(self):
+        self.image_view.toggleDragMode()
+
+    def photoClicked(self, pos):
+        if self.image_view.dragMode()  == QtWidgets.QGraphicsView.NoDrag:
+            for i in range(self.photo_details_listbox.count()):
+                item = self.photo_details_listbox.item(i)
+                prop = item.data(Qt.UserRole)
+                if prop.name == "Position of cursor: ":
+                    prop.value = '%d, %d' % (pos.x(), pos.y())
+                    item.setText(prop.name + prop.value)
 
     def create_bar_graph(self, data):
         # Clear the current axes, necessary if you want to update the graph
@@ -82,9 +192,9 @@ class MainWindow(QMainWindow):
         ax.xaxis.label.set_color('white')
         ax.yaxis.label.set_color('white')
 
-        ax.tick_params(colors='black')
-        ax.xaxis.label.set_color('black')
-        ax.yaxis.label.set_color('black')
+        # ax.tick_params(colors='black')
+        # ax.xaxis.label.set_color('black')
+        # ax.yaxis.label.set_color('black')
 
         # Label the bars
         labels = ['NORMAL', 'PNEUMONIA']
@@ -134,18 +244,39 @@ class MainWindow(QMainWindow):
     def load_selected_image(self):
         selected_file = self.file_listbox.currentItem().text()
         file_path = os.path.join(self.folder_path, selected_file)
-        self.display_image(file_path)
-        self.call_classify_image(file_path)
+        if os.path.splitext(file_path)[1].lower() in ['.png', '.jpg', '.jpeg', '.bmp', '.gif', '.tiff']:
+            try:
+                file_path = os.path.join(self.folder_path, selected_file)
+                self.display_image(file_path)
+                self.pixInfo()
+                self.call_classify_image(file_path)
+            except IOError:
+                print(f"{file_path} is not a valid image file.")
+                return
 
     def display_image(self, file_path):
-        pixmap = QPixmap(file_path)
-        self.image_view._photo.setPixmap(pixmap)
-        self.image_view.fitInView()
+        pix = QPixmap(file_path)
+        self.image_view.setPhoto(pix)
+        for i in range(self.photo_details_listbox.count()):
+            item = self.photo_details_listbox.item(i)
+            prop = item.data(Qt.UserRole)
+            if prop.name == "Image size: ":
+                prop.value = '%d, %d' % (pix.width(), pix.height())
+                item.setText(prop.name + prop.value)
 
     def call_classify_image(self, file_path):
         var = callable.classify_image(file_path)
         print(var)
         self.create_bar_graph(var)
+        for i in range(self.photo_details_listbox.count()):
+            item = self.photo_details_listbox.item(i)
+            prop = item.data(Qt.UserRole)
+            if prop.name == "Result: ":
+                if var[0] > var[1]:
+                    prop.value = '%s' % ("Normal")
+                else:
+                    prop.value = '%s' % ("Pneumonia")
+                item.setText(prop.name + prop.value)
 
 if __name__ == "__main__":
     app = QApplication([])
